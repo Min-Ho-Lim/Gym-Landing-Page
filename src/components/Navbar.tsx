b1944676a2f03a6f94a8ccb367bfffee8abd0ee7
fahim1996112016 <fahimul.huq85@gmail.com>
2022-12-09 16:23:01 +0600

    toggle hamburger menu

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,7 +1,12 @@
-import React from "react";
+import React,{useState} from "react";
 import { GymName } from "../constants";
-
 const Navbar = () => {
+// set the default dropmenu as false because we want to hide it in smaller screen
+const[dropMenu, setDropMenu] = useState(false) 
+// function for toggling drop menu
+const toggleDropMenu=()=>{
+setDropMenu(!dropMenu)
+}
   return (
     // create tailwind navbar
     <nav className="flex items-center justify-between flex-wrap bg-primary p-6">
@@ -17,12 +22,14 @@ const Navbar = () => {
           <path d="M27 9c-9.374 0-17 7.626-17 17s7.626 17 17 17 17-7.626 17-17-7.626-17-17-17zm0 30c-7.18 0-13-5.82-13-13s5.82-13 13-13 13 5.82 13 13-5.82 13-13 13z" />
           <path d="M27 18c-4.411 0-8 3.589-8 8s3.589 8 8 8 8-3.589 8-8-3.589-8-8-8zm0 13c-2.757 0-5-2.243-5-5s2.243-5 5-5 5 2.243 5 5-2.243 5-5 5z" />
         </svg>
+        
         <span className="font-poppins font-semibold text-3xl tracking-tight">
           {GymName}
         </span>
+       
       </div>
       <div className="block lg:hidden">
-        <button className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
+        <button onClick={toggleDropMenu} className="flex items-center px-3 py-2 border rounded text-gray-200 border-gray-400 hover:text-white hover:border-white">
           <svg
             className="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -32,10 +39,11 @@ const Navbar = () => {
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
         </button>
+    
       </div>
       {/* tailwind center items */}
 
-      <div className="w-full block flex-grow lg:flex lg:items-center lg:w-auto lg:pl-5">
+      <div className={`w-full block flex-grow  lg:flex lg:items-center lg:w-auto lg:pl-5  ${dropMenu ? (`block lg:hidden`):(`hidden lg:block`)}`} >
         <div className="font-poppins text-sm lg:flex-grow lg:justify-items-center">
           <a
             href="#responsive-header"
