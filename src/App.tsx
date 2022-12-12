ee828b76e7fb11f266f08de0595e3d68c64bec59
DonUggioni <renan.uggioni@gmail.com>
2022-12-12 18:03:33 +0000

    Created loading page

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ import Navbar from './components/Navbar';
 const App = () => {
   return (
     <div className="w-screen bg-primary">
-      <LoadingScreen />
       <Navbar />
       <Main />
       <Features />
--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -4,7 +4,7 @@ import { ClipLoader } from 'react-spinners';
 
 export const LoadingScreen = () => {
   return (
-    <div className=" absolute z-10 top-0 left-0 w-full h-screen bg-gray-50/90 flex justify-center items-center">
+    <div className=" absolute z-20 top-0 left-0 w-full h-screen bg-gray-50/95 flex justify-center items-center">
       <div className="flex flex-col gap-y-5 justify-center items-center">
         <ClipLoader color="#112048" size={50} speedMultiplier={0.8} />
         <p className="text-lg text-[#112048]">Loading...</p>
