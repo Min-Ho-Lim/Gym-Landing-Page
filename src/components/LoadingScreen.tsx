404805435b37da3ea53cfa387839391398e49e49
DonUggioni <renan.uggioni@gmail.com>
2022-12-12 17:55:12 +0000

    Created loading page

---
+++ src/components/LoadingScreen.tsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+import { ClipLoader } from 'react-spinners';
+
+export const LoadingScreen = () => {
+  return (
+    <div className=" absolute z-10 top-0 left-0 w-full h-screen bg-gray-50/90 flex justify-center items-center">
+      <div className="flex flex-col gap-y-5 justify-center items-center">
+        <ClipLoader color="#112048" size={50} speedMultiplier={0.8} />
+        <p className="text-lg text-[#112048]">Loading...</p>
+      </div>
+    </div>
+  );
+};
