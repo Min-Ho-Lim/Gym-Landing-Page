5926b4e5f51718dc8886f85ec1c18ad768785b68
DonUggioni <renan.uggioni@gmail.com>
2022-12-13 14:48:06 +0000

    Redone loading page, removed react-spinner package

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,14 @@ import React from 'react';
 import BlogPosts from './components/BlogPosts';
 import Features from './components/Features';
 import JoinUs from './components/JoinUs';
-import { LoadingScreen } from './components/LoadingScreen';
 import Main from './components/Main';
 import Navbar from './components/Navbar';
+import { LoadingScreen } from './components/LoadingScreen';
 
 const App = () => {
   return (
     <div className="w-screen bg-primary">
+      <LoadingScreen />
       <Navbar />
       <Main />
       <Features />
--- src/components/LoadingScreen.tsx
+++ src/components/LoadingScreen.tsx
@@ -1,13 +1,19 @@
 import React from 'react';
 
-import { ClipLoader } from 'react-spinners';
+import loadingSpinner from '../assets/dumbbell_loading_spinner.svg';
 
 export const LoadingScreen = () => {
   return (
-    <div className=" absolute z-20 top-0 left-0 w-full h-screen bg-gray-50/95 flex justify-center items-center">
-      <div className="flex flex-col gap-y-5 justify-center items-center">
-        <ClipLoader color="#112048" size={50} speedMultiplier={0.8} />
-        <p className="text-lg text-[#112048]">Loading...</p>
+    <div className="w-full h-screen flex items-center justify-center bg-zinc-900">
+      <div className="flex flex-col items-center justify-center gap-10 w-24 animate-pulse">
+        <img
+          src={loadingSpinner}
+          alt="Loading spinner"
+          className="animate-spin-slow"
+        />
+        <p className="animate-pulse text-2xl font-poppins tracking-wider text-white">
+          Loading...
+        </p>
       </div>
     </div>
   );
